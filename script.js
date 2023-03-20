f0e4be14b2d5f328a81f55936d2d7e7496fbd1bb
Animesh Gupta <54591103+animeshgupta971@users.noreply.github.com>
2023-03-21 02:13:36 +0530

    changes

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("sticky");
+        } else {
+           $(".header").removeClass("sticky");
+        }
+    });
+});
