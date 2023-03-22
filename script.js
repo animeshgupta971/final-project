9c5e87c6b18e40916e6e6e110f0f964a37c51274
Animesh Gupta <54591103+animeshgupta971@users.noreply.github.com>
2023-03-23 02:00:37 +0530

    changes

--- script.js
+++ script.js
@@ -7,3 +7,80 @@ $(function() {
         }
     });
 });
+
+
+
+ $(document).ready(function() {
+    $(".merox-variants").click(function() {
+        $(".merox-variants").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('.cartBtn').click(function() {
+        var thiss = $(this);
+        var id = $(this).parents().find(".merox-variants.active").attr('data-var-id');
+        addItemToCart(id, thiss);
+    });
+   
+    function addItemToCart(variant_id, thiss) {
+        jQuery.ajax({
+            type: 'POST',
+            url: '/cart/add.js',
+            data: {
+                "id": variant_id,
+                "quantity": 1
+            },
+            dataType: 'json',
+            success: function() {
+                thiss.text("ADDED")
+                window.location = "/cart";
+            }
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
